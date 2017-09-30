25bb280eaa1159811ccb374ce65c67d3a27fd23f
Anderson Sipe <amsipe@gmail.com>
2017-09-30 16:08:04 -0400

    add form for messages

--- server/server.js
+++ server/server.js
@@ -19,15 +19,16 @@ io.on('connection', (socket) => {
 
     socket.broadcast.emit('newMessage',generateMessage('Admin', 'A new user has joined.'));
 
-    socket.on('createMessage',(message) => {
+    socket.on('createMessage',(message, callback) => {
         io.emit('newMessage', generateMessage(message.from,message.text));
-
+        callback('This is from the server.');
         // socket.broadcast.emit('newMessage',{
         //     from: message.from,
         //     text: message.text,
         //     createdAt: new Date().getTime()
         // })
     })
+
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     })
